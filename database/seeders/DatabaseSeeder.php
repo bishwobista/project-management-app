785bf349901d5acdd27e0bb9d1978240fbe9acb6
Bishwo Bista <bishwo5bista@gmail.com>
2024-07-31 13:56:40 +0545

    task and project factory migration

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Project;
 
 class DatabaseSeeder extends Seeder
 {
@@ -16,8 +17,24 @@ class DatabaseSeeder extends Seeder
         // User::factory(10)->create();
 
         User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+            'id' => 1,
+            'name' => 'dave',
+            'email' => 'dave@malinator.com',
+            'password' => bcrypt('password'),
+            'email_verified_at' => time()
         ]);
+        User::factory()->create([
+            'id' => 2,
+            'name' => 'Bishwo',
+            'email' => 'bishwo@malinator.com',
+            'password' => bcrypt('password'),
+            'email_verified_at' => time()
+        ]);
+
+        Project::factory()
+            ->count(30)
+            ->hasTasks(30)
+            ->create();
+
     }
 }
