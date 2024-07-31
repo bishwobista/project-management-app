8786e095a8b3d079cbc7ca8ffc8924e291717878
Bishwo Bista <bishwo5bista@gmail.com>
2024-07-31 14:00:11 +0545

    typo fix

--- database/migrations/2024_07_30_051824_create_tasks_table.php
+++ database/migrations/2024_07_30_051824_create_tasks_table.php
@@ -20,9 +20,9 @@ return new class extends Migration
             $table->string('priority');
             $table->string('due_date')->nullable();
             $table->foreignId('assigned_user_id')->constrained('users');
-            $table->forignId('created_by')->constrained('users');
-            $table->forignId('updated_by')->constrained('users');
-            $table->forignId('project_id')->constrained('projects');
+            $table->foreignId('created_by')->constrained('users');
+            $table->foreignId('updated_by')->constrained('users');
+            $table->foreignId('project_id')->constrained('projects');
             $table->timestamps();
         });
     }
